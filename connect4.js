01eb9939452228291c26ee54bde6a85ef59a7c00
Eli <cyronix787@gmail.com>
2020-11-30 12:57:06 +0000

    change current player after click

--- connect4.js
+++ connect4.js
@@ -1,4 +1,4 @@
-var tableflow = document.getElementsByTagName("tr");
+var tableRow = document.getElementsByTagName("tr");
 var tablecell = document.getElementsByTagName("td");
 var tableslot = document.querySelector(".slot");
 const playerTurn = document.querySelector(".Your-Turn");
@@ -7,13 +7,15 @@ const turnDisc = playerTurn.previousElementSibling;
 const player1NameElem = document.querySelector("#player-1-name");
 const player2NameElem = document.querySelector("#player-2-name");
 var player1, player2, player1Color, player2Color;
+var currentPlayer = 1;
 
-function changePlayerTurn(currentPlayer) {
+function changePlayerTurn(current) {
+  currentPlayer = current;
   function setColorAndTurn(playerName, playerColor) {
     turnDisc.style.backgroundColor = playerColor;
     playerTurn.textContent = `${playerName}'s turn`;
   }
-  switch (currentPlayer) {
+  switch (current) {
     case 1: {
       setColorAndTurn(player1, player1Color);
       break;
@@ -27,15 +29,9 @@ function changePlayerTurn(currentPlayer) {
   }
 }
 
-Array.prototype.forEach.call(tablecell, (cell) => {
-  cell.addEventListener("click", changeColor);
-  cell.style.backgroundColor = "white";
-});
-
 function changeColor(e) {
   let column = e.target.cellIndex;
   let row = [];
-
   for (let i = 5; i > -1; i--) {
     if (tableRow[i].children[column].style.backgroundColor == "white") {
       row.push(tableRow[i].children[column]);
@@ -43,6 +39,7 @@ function changeColor(e) {
         row[0].style.backgroundColor = player1Color;
         return changePlayerTurn(2);
       }
+      console.log({ player2Color });
       row[0].style.backgroundColor = player2Color;
       return changePlayerTurn(1);
     }
@@ -50,8 +47,13 @@ function changeColor(e) {
 }
 
 function startGame() {
+  Array.prototype.forEach.call(tablecell, (cell) => {
+    cell.addEventListener("click", changeColor);
+    cell.style.backgroundColor = "white";
+  });
+
   for (let i = 0; i < tablecell.length; i++) {
-    tablecell[i].addEventListener(".click", (e) => {
+    tablecell[i].addEventListener("click", (e) => {
       console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
     });
   }
@@ -61,16 +63,13 @@ function startGame() {
     player1NameElem.textContent = player1;
   }
 
-  player1Color = "#004cda";
-
   if (!player2) {
     player2 = prompt("Player two: Enter your name, you will be yellow");
     player2NameElem.textContent = player2;
   }
 
+  player1Color = "#004cda";
   player2Color = "#fffb00";
-
-  var currentPlayer = 1;
   changePlayerTurn(currentPlayer);
 }
 
