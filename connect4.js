35b401dc146733d3327add3602bb1b784d80410d
Eli <cyronix787@gmail.com>
2020-11-30 13:32:28 +0000

    allow wins horizontally and vertically

--- connect4.js
+++ connect4.js
@@ -8,6 +8,8 @@ const player1NameElem = document.querySelector("#player-1-name");
 const player2NameElem = document.querySelector("#player-2-name");
 var player1, player2, player1Color, player2Color;
 var currentPlayer = 1;
+var player1Spots = [];
+var player2Spots = [];
 
 function changePlayerTurn(current) {
   currentPlayer = current;
@@ -31,20 +33,64 @@ function changePlayerTurn(current) {
 
 function changeColor(e) {
   let column = e.target.cellIndex;
-  let row = [];
+  let rows = [];
   for (let i = 5; i > -1; i--) {
     if (tableRow[i].children[column].style.backgroundColor == "white") {
-      row.push(tableRow[i].children[column]);
+      rows.push(tableRow[i].children[column]);
+      const row = rows[0].parentElement.rowIndex;
       if (currentPlayer === 1) {
-        row[0].style.backgroundColor = player1Color;
+        rows[0].style.backgroundColor = player1Color;
+        player1Spots.push([row, column]);
+        checkWinner();
         return changePlayerTurn(2);
       }
-      console.log({ player2Color });
-      row[0].style.backgroundColor = player2Color;
+      rows[0].style.backgroundColor = player2Color;
+      player2Spots.push([row, column]);
+      checkWinner();
       return changePlayerTurn(1);
     }
   }
 }
+
+function checkWinner() {
+  console.log({ player1Spots, player2Spots });
+  let player1Win = 1;
+  let player2Win = 1;
+  if (player1Spots.length >= 4) {
+    let player1PreviousRow = player1Spots[0][0];
+    let player1PreviousColumn = player1Spots[0][1];
+    player1Spots.forEach(function ([row, column]) {
+      player1Win +=
+        Math.abs(player1PreviousRow - row) +
+        Math.abs(player1PreviousColumn - column);
+      player1PreviousRow = row;
+      player1PreviousColumn = column;
+    });
+    if (player1Win === 4) {
+      playerTurn.textContent = `${player1} wins!`;
+      console.log(`${player1} wins!`);
+    }
+  }
+  if (player2Spots.length >= 4) {
+    let player2PreviousRow = player2Spots[0][0];
+    let player2PreviousColumn = player2Spots[0][1];
+    player2Spots.forEach(function ([row, column]) {
+      player2Win +=
+        Math.abs(player2PreviousRow - row) +
+        Math.abs(player2PreviousColumn - column);
+      player2PreviousRow = row;
+      player2PreviousColumn = column;
+    });
+    if (player2Win === 4) {
+      playerTurn.textContent = `${player2} wins!`;
+      console.log(`${player2} wins!`);
+    }
+  }
+}
+
+function disableBoard() {}
+
+function resetBoard() {}
 
 function startGame() {
   Array.prototype.forEach.call(tablecell, (cell) => {
@@ -54,7 +100,9 @@ function startGame() {
 
   for (let i = 0; i < tablecell.length; i++) {
     tablecell[i].addEventListener("click", (e) => {
-      console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+      const row = e.target.parentElement.rowIndex;
+      const column = e.target.cellIndex;
+      console.log(`${row}, ${column}`);
     });
   }
 
